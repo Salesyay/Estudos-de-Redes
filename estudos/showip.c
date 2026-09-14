ee1fd48a289f0130639d86614e921972c0e77689
Salesyay <berenandluthienyay@proton.me>
2026-09-13 22:28:59 -0300

    Atualizacao programa showip

--- estudos/showip.c
+++ estudos/showip.c
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
     }
 
     memset(&hints, 0, sizeof hints);
+
     hints.ai_family = AF_UNSPEC;     // IPv4 ou IPv6
     hints.ai_socktype = SOCK_STREAM; // TCP
 
@@ -38,16 +39,29 @@ int main(int argc, char *argv[])
 
         void *addr;
         char *ipver;
+        struct sockaddr_in *ipv4;
+        struct sockaddr_in6 *ipv6;
 
         if (p->ai_family == AF_INET) { // IPv4
-            struct sockaddr_in *ipv4 =
-                (struct sockaddr_in *)p->ai_addr;
+
+            ipv4 = (struct sockaddr_in *)p->ai_addr;
 
             addr = &(ipv4->sin_addr);
             ipver = "IPv4";
 
         } else { // IPv6
-            // tratar endereço IPv6 aqui
+
+            ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
         }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf(" %s: %s\n", ipver, ipstr);
     }
+
+    freeaddrinfo(res);
+
+    return 0;
 }
